6bf13d1ea75b1c9047e0325fe78b3bc35c1d228e
Dennis Langer <dennis.d.langer@ntnu.no>
2021-05-03 19:17:53 +0200

    Add fragment shader for monochromatic textures

---
+++ shaders/pos-uv-mono_frag.glsl
@@ -0,0 +1,13 @@
+#version 330 core
+
+in vec2 fragmentUV;
+
+out vec4 color;
+
+uniform sampler2D textureSampler;
+
+void main()
+{
+    //color = vec4(fragmentUV, texture(textureSampler, fragmentUV).b, 1.0);
+    color = vec4(vec3(2.0*texture(textureSampler, fragmentUV).r), 1.0);
+}
